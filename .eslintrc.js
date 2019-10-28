57fbf433b53aea25fa349c629b36e767fde3b280
Hendrik Lammers <lammers@gmail.com>
2019-10-28 20:08:10 +0700

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  env: {
+    browser: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+  globals: {
+    figma: true,
+    __html__: true,
+  },
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# figma-plugin-elm [![Build Status](https://travis-ci.org/hendriklammers/figma-plugin-elm.svg?branch=master)](https://travis-ci.org/hendriklammers/figma-plugin-elm)
+
+Figma plugin to export SVG as Elm code
+
+
+## Install
+
+```
+
+```
+
+## Usage
+
+```
+
+```
+
+## License
+
+MIT
