4236803f968c36f6cc31269a95caa730c91c040d
Hendrik Lammers <lammers@gmail.com>
2019-11-20 20:32:27 +0700

    Use scss and update ui and styling

--- src/ui.ts
+++ src/ui.ts
@@ -1,12 +1,13 @@
 import Clipboard from 'clipboard'
-import './ui.css'
+import './ui.scss'
 
 new Clipboard('#copy-button')
 
 window.onmessage = (event: MessageEvent) => {
   const msg = event.data.pluginMessage
-  if (msg && msg.type === 'show-code') {
-    const codeElement = document.getElementById('code')
+
+  if (msg?.type === 'show-code') {
+    const codeElement = document.getElementById('code-output')
     codeElement.textContent = msg.data
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -24,8 +24,8 @@ module.exports = (env, argv) => ({
 
       // Enables including CSS by doing "import './file.css'" in your TypeScript code
       {
-        test: /\.css$/,
-        loader: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
 
       // Allows you to use "<%= require('./file.svg') %>" in your HTML code to get a data URI
