a72ec5abcbdaea20aa4c8ec39962323df77d2ba0
Hendrik Lammers <lammers@gmail.com>
2019-11-21 20:51:06 +0700

    Apply aliases to html-elm options

--- src/code.ts
+++ src/code.ts
@@ -1,14 +1,9 @@
-import convertToElm from 'html-elm'
+import htmlToElm, { Options } from 'html-elm'
 
 const ab2str = (buf: ArrayBuffer) =>
   String.fromCharCode.apply(null, new Uint16Array(buf))
 
-// Allow the plugin to run when only 1 element is selected
-if (figma.currentPage.selection.length !== 1) {
-  figma.closePlugin()
-}
-
-;(async () => {
+const convertToElm = async (options: Options = { imports: true }) => {
   try {
     const selection = figma.currentPage.selection.map(async element => {
       const buffer = await element.exportAsync({ format: 'SVG' })
@@ -16,11 +11,36 @@ if (figma.currentPage.selection.length !== 1) {
     })
 
     const arr = await Promise.all(selection)
-    const data = convertToElm(arr.join('\n'))
-    figma.showUI(__html__, { width: 600, height: 400 })
+    const data = await htmlToElm(arr.join('\n'), options)
+
     figma.ui.postMessage({ type: 'show-code', data })
   } catch (err) {
     console.error(err)
     figma.closePlugin()
   }
-})()
+}
+
+// Allow the plugin to run when only 1 element is selected
+// TODO: Show warning in ui?
+if (figma.currentPage.selection.length !== 1) {
+  figma.closePlugin()
+}
+
+figma.showUI(__html__, { width: 600, height: 400 })
+
+convertToElm()
+
+figma.ui.onmessage = msg => {
+  switch (msg.type) {
+    case 'cancel':
+      figma.closePlugin()
+      break
+    case 'apply-settings':
+      convertToElm({ imports: true, ...msg.options })
+      break
+    default:
+      console.error('Invalid message type')
+      figma.closePlugin()
+  }
+}
+
--- src/ui.ts
+++ src/ui.ts
@@ -1,13 +1,52 @@
 import Clipboard from 'clipboard'
+import { Options } from 'html-elm'
 import './ui.scss'
 
+// Use Clipboard.js lib to add Copy to Clipboard functionality
 new Clipboard('#copy-button')
 
 window.onmessage = (event: MessageEvent) => {
   const msg = event.data.pluginMessage
-
   if (msg?.type === 'show-code') {
-    const codeElement = document.getElementById('code-output')
+    const codeElement = document.querySelector('#code-output')
     codeElement.textContent = msg.data
   }
 }
+
+document.querySelector('#cancel-button').addEventListener('click', () => {
+  parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*')
+})
+
+const applySettingsButton: HTMLButtonElement = document.querySelector(
+  '#apply-settings-button'
+)
+const aliasSvgInput: HTMLInputElement = document.querySelector('#alias-svg')
+const aliasSvgAttributeInput: HTMLInputElement = document.querySelector(
+  '#alias-svg-attr'
+)
+
+let options: Options = {}
+
+applySettingsButton.addEventListener('click', () => {
+  if (options) {
+    parent.postMessage(
+      { pluginMessage: { type: 'apply-settings', options } },
+      '*'
+    )
+  }
+})
+
+document.querySelectorAll('#alias-svg, #alias-svg-attr').forEach(elem => {
+  elem.addEventListener('input', () => {
+    options = {
+      svgAlias: aliasSvgInput.value,
+      svgAttributeAlias: aliasSvgAttributeInput.value,
+    }
+
+    if (Object.values(options).filter(val => val).length) {
+      applySettingsButton.disabled = false
+    } else {
+      applySettingsButton.disabled = true
+    }
+  })
+})
