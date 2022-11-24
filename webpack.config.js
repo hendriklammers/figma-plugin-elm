e1a84eaa2276713f234ef7c0a2fda1a480357422
Hendrik Lammers <lammers@gmail.com>
2022-11-24 22:23:50 +0800

    Update build

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,71 @@
 const InlineChunkHtmlPlugin = require('react-dev-utils/InlineChunkHtmlPlugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 const path = require('path')
 
-module.exports = (_env, argv) => ({
-  mode: argv.mode === 'production' ? 'production' : 'development',
+module.exports = (_env, argv) => {
+  const mode = argv.mode === 'production' ? 'production' : 'development'
 
-  // This is necessary because Figma's 'eval' works differently than normal eval
-  devtool: argv.mode === 'production' ? false : 'inline-source-map',
+  return {
+    mode,
 
-  entry: {
-    ui: './src/ui.ts', // The entry point for your UI code
-    code: './src/code.ts', // The entry point for your plugin code
-  },
+    // This is necessary because Figma's 'eval' works differently than normal eval
+    devtool: argv.mode === 'production' ? false : 'inline-source-map',
 
-  module: {
-    rules: [
-      // Converts TypeScript code to JavaScript
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
+    entry: {
+      ui: './src/ui.ts', // The entry point for your UI code
+      code: './src/code.ts', // The entry point for your plugin code
+    },
 
-      // Enables including CSS by doing "import './file.css'" in your TypeScript code
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
+    module: {
+      rules: [
+        // Converts TypeScript code to JavaScript
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+
+        // Enables including CSS by doing "import './file.css'" in your TypeScript code
+        {
+          test: /\.scss$/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+      ],
+    },
+
+    // Webpack tries these extensions for you if you omit the extension like "import './file'"
+    resolve: { extensions: ['.tsx', '.ts', '.jsx', '.js'] },
+
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist'), // Compile into a folder called "dist"
+    },
+
+    // Tells Webpack to generate "ui.html" and to inline "ui.ts" into it
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/ui.html',
+        filename: 'ui.html',
+        inlineSource: '.(js)$',
+        chunks: ['ui'],
+        inject: 'body',
+      }),
+      new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/ui/]),
     ],
-  },
 
-  // Webpack tries these extensions for you if you omit the extension like "import './file'"
-  resolve: { extensions: ['.tsx', '.ts', '.jsx', '.js'] },
-
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'), // Compile into a folder called "dist"
-  },
-
-  // Tells Webpack to generate "ui.html" and to inline "ui.ts" into it
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/ui.html',
-      filename: 'ui.html',
-      inlineSource: '.(js)$',
-      chunks: ['ui'],
-    }),
-    new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/ui/]),
-  ],
-})
+    optimization: {
+      minimize: mode === 'production' ? true : false,
+      minimizer: [
+        new TerserPlugin({
+          extractComments: false,
+          terserOptions: {
+            format: {
+              comments: false,
+            },
+          },
+        }),
+      ],
+    },
+  }
+}
